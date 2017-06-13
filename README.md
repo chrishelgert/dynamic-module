be27c7bec9e30f1911a506e7017517ef1a2fb4ae
Chris Helgert <chris.helgert@online.de>
2017-06-13 21:12:14 +0200

    Inital commit 🐣

---
+++ README.md
@@ -0,0 +1,25 @@
+# dynamic-module
+
+[![npm version](https://badge.fury.io/js/dynamic-module.svg)](https://badge.fury.io/js/dynamic-module)
+[![Build Status](https://travis-ci.org/chrishelgert/dynamic-module.svg?branch=master)](https://travis-ci.org/chrishelgert/dynamic-module)
+[![codecov](https://codecov.io/gh/chrishelgert/dynamic-module/branch/master/graph/badge.svg)](https://codecov.io/gh/chrishelgert/dynamic-module)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![Greenkeeper badge](https://badges.greenkeeper.io/chrishelgert/dynamic-module.svg)](https://greenkeeper.io/)
+
+> install and use modules only if you need them
+
+## Usage
+
+TODO
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch
+3. Commit your changes
+4. Push to the branch
+5. Create new Pull Request
+
+## LICENSE
+
+Copyright (c) 2017 Chris Helgert. See [License](./LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const shell = require('shelljs')
+
+/**
+ * Install the dependeny with yarn or npm
+ *
+ * @param  {string} moduleName name of the module
+ * @return {object}            shelljs result with code, stderr and stdout
+ */
+const installDependency = (moduleName) => {
+  if (shell.which('yarn')) {
+    // install it with yarn
+    return shell.exec(`yarn add ${moduleName}`, { silent: true })
+  }
+
+  // install it with npm
+  return shell.exec(`npm install ${moduleName}`, { silent: true })
+}
+
+/**
+ * resolves the path to the node_modules
+ *
+ * @param  {string} moduleName name of the module
+ * @return {any}               resolved node_module
+ */
+const resolveNodeModule = (moduleName) => require(path.join(process.cwd(), 'node_modules', moduleName))
+
+/**
+ * dynamic module
+ * returns the required module
+ * or install it and require the module
+ *
+ * @param  {string}  moduleName name of the module
+ * @return {promise}            promise with the module
+ */
+module.exports = (moduleName) => new Promise((resolve, reject) => {
+  try {
+    // is it already installed
+    resolve(resolveNodeModule(moduleName))
+  } catch (err) {
+    console.log(`module '${moduleName}' is not installed. Let´s install it`)
+  }
+
+  // install dependency
+  if (installDependency(moduleName).code !== 0) {
+    // error while installing the module
+    reject(new Error(`failed to install ${moduleName}`))
+  }
+
+  // return required module
+  resolve(resolveNodeModule(moduleName))
+})
